e53fa7dc0da68cc4ba64cc60d20956447ff5f219
halil-akgun <halilakgun0@gmail.com>
2023-09-17 15:36:40 +0300

    day01-day02

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import X from "./components/01-my-house/01-my-house";
+import { Armutlar, Muzlar } from "./components/01-my-house/01-my-house";
+import Jsx1 from "./components/02-jsx/01-jsx";
+import Jsx2 from "./components/02-jsx/02-jsx";
+import Jsx3 from "./components/02-jsx/03-jsx";
+import Jsx4 from "./components/02-jsx/04-jsx";
+
+const App = () => {
+    return (
+        <div>
+            <X />
+            <Armutlar />
+            <Muzlar />
+            <Jsx1 />
+            <Jsx2 />
+            <Jsx3 />
+            <Jsx4 />
+        </div>
+    )
+};
+
+export default App;
